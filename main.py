be01dacd48168e448ed85948da5727b87dc68e2b
aditya-m-mishra <adiapplies@gmail.com>
2026-04-15 16:35:13 +0530

    score/level or number of correct guesses added

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ guessed_states = []
 
 while len(guessed_states) < 50:
 
-    answer_state = screen.textinput(title="Guess the state", prompt="name the states")
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 states are correct",
+        prompt="what's another state's name",
+    )
 
     if answer_state is None:
         break
