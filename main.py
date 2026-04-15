8a06c5a449a2a792871159a5e7d23c46d153c82d
aditya-m-mishra <adiapplies@gmail.com>
2026-04-15 15:12:14 +0530

    Input prompt added

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+answer_state = screen.textinput(
+    title="Guess the state", prompt="What's this state's name"
+)
+
+print(answer_state)
